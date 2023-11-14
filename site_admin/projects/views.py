ea1f3091cdf3d1481ff4077830152491daec77a2
Paul McKillop <pgmckillop@gmail.com>
2023-11-14 17:29:07 +0000

    Base two-page build complete and tested.

--- site_admin/projects/views.py
+++ site_admin/projects/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'projects/index.html', {})
+
+
+def about(request):
+    return render(request, 'projects/about.html', {})
